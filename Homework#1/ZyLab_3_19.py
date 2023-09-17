4ab570acfcba10df9c4e97d7358ae27e4b7d76b7
huzaifashoeb <307huzaifa@gmail.com>
2023-09-17 17:26:43 -0500

    ZyLab 3.19

--- Homework#1/ZyLab_3_19.py
+++ Homework#1/ZyLab_3_19.py
@@ -1,12 +1,22 @@
 import math
-wall_height = float(input('Enter wall height (feet):\n'))
-wall_width = float (input('Enter wall width (feet):\n'))
-wall_area = float(wall_height * wall_width)
+wall_height = int(input('Enter wall height (feet):\n'))
+wall_width = int(input('Enter wall width (feet):\n'))
+wall_area = int(wall_height * wall_width)
 
-print('Wall area:','{:.2f}'.format(wall_area),'square feet')
+print('Wall area:',wall_area,'square feet')
 
 paint_needed = float(wall_area/350)
-print('Paint needed:', '{:.2f}'.format(paint_needed), 'gallons')
+print('Paint needed:','{:.2f}'.format(paint_needed), 'gallons')
 
 cans = math.ceil(paint_needed)
-print('Cans needed:',cans,'can(s)')
+print('Cans needed:',cans,'can(s)\n')
+
+
+color_dict={'red':35,'blue':25,'green':23}
+
+color_wanted = input('Choose a color to paint the wall:\n')
+
+price = cans * color_dict[color_wanted]
+
+print('Cost of purchasing',color_wanted,'paint: $',end='')
+print(price)

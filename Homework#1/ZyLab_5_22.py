a478ffbc19772bf0b9de08d07778d67a1ffb0020
huzaifashoeb <307huzaifa@gmail.com>
2023-09-17 19:18:18 -0500

    ZyLab 5.22

--- Homework#1/ZyLab_5_22.py
+++ Homework#1/ZyLab_5_22.py
@@ -1,1 +1,37 @@
+print("Davy's auto shop services")
+print('Oil change -- $35')
+print('Tire rotation -- $19')
+print('Car wash -- $7')
+print('Car wax -- $12\n')
 
+services_dict = {'Oil change': 35, 'Tire rotation': 19, 'Car wash': 7, 'Car wax': 12}
+
+first = input('Select first service:\n')
+second = input('Select second service:\n\n')
+
+# now the invoice part
+
+print("Davy's auto shop invoice\n")
+
+if first == '-':
+    print('Service 1: No service')
+
+else:
+    print('Service 1:', first, end='')
+    print(', $', end='')
+    print(services_dict[first])
+
+if second == '-':
+    print('Service 2: No service')
+    print('')
+    print('Total: $', end='')
+    print(services_dict[first])
+
+else:
+    print('Service 2:', second, end='')
+    print(', $', end='')
+    print(services_dict[second])
+    print('')
+    total = int(services_dict[first] + services_dict[second])
+    print('Total: $', end='')
+    print(total)

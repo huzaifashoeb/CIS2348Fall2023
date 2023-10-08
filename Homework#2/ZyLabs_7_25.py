072f5e1a428be1375c89da34051901990ddb8ddf
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 17:30:18 -0500

    Update ZyLabs_7_25.py

--- Homework#2/ZyLabs_7_25.py
+++ Homework#2/ZyLabs_7_25.py
@@ -1,41 +1,40 @@
 # Name: Huzaifa Shoeb, ID: 1925670
 
-amount = int(input())
+ef exact_change():
+    input_val = int(input(''))
+    if input_val <= 0:
+        print('no change')
 
-dollars = amount // 100
-quarters = amount % 100 // 25
-dimes = amount % 100 % 25 // 10
-nickels = amount % 100 % 25 % 10 // 5
-pennies = amount % 100 % 25 % 10 % 5 // 1
+    else:
+        num_dollars = input_val // 100
+        input_val %= 100
+        num_quarters = input_val // 25
+        input_val %= 25
+        num_dimes = input_val // 10
+        input_val %= 10
+        num_nickels = input_val // 5
+        input_val %= 5
+        num_pennies = input_val
 
-if amount <= 0:
-    print("No change")
-  
-if dollars > 1:
-    print(str(dollars) + " Dollars")
-elif dollars == 1:
-    print(str(dollars) + " Dollar")
-else:
-    pass
-if quarters > 1:
-    print(str(quarters) + " Quarters")
-elif quarters == 1:
-    print(str(quarters) + " Quarter")
-else:
-    pass
-if dimes > 1:
-    print(str(dimes) + " Dimes")
-elif dimes == 1:
-    print(str(dimes) + " Dime")
-else:
-    pass
-if nickels > 1:
-    print(str(nickels) + " Nickels")
-elif nickels == 1:
-    print(str(nickels) + " Nickel")
-else:
-    pass
-if pennies > 1:
-    print(str(pennies) + " Pennies")
-elif pennies == 1:
-    print(str(pennies) + " Penny")
+        if num_dollars > 1:
+            print('%d dollars' % num_dollars)
+        elif num_dollars == 1:
+            print('%d dollar' % num_dollars)
+        if num_quarters > 1:
+            print('%d quarters' % num_quarters)
+        elif num_quarters == 1:
+            print('%d quarter' % num_quarters)
+        if num_dimes > 1:
+            print('%d dimes' % num_dimes)
+        elif num_dimes == 1:
+            print('%d dime' % num_dimes)
+        if num_nickels > 1:
+            print('%d nickels' % num_nickels)
+        elif num_nickels == 1:
+            print('%d nickel' % num_nickels)
+        if num_pennies > 1:
+            print('%d pennies' % num_pennies)
+        elif num_pennies == 1:
+            print('%d penny' % num_pennies)
+
+exact_change()

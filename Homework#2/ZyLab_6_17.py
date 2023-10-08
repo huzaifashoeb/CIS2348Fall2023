efa8b2420c5d9b732ae9a0e46bd1c1517351afca
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 14:10:11 -0500

    Update ZyLab_6_17.py

--- Homework#2/ZyLab_6_17.py
+++ Homework#2/ZyLab_6_17.py
@@ -1,1 +1,13 @@
-#Name: Huzaifa Shoeb, ID: 1925670
+# Name: Huzaifa Shoeb, ID: 1925670
+
+word = input()
+
+word = word.replace('i', '!')
+word = word.replace('a', '@')
+word = word.replace('m', 'M')
+word = word.replace('B', '8')
+word = word.replace('o', '.')
+word += 'q*s'
+
+password = word
+print(password)

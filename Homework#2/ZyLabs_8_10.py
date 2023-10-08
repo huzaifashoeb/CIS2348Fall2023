5cd9a22f5e41e57e52ad7b0ff746f50465c758c3
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 18:09:41 -0500

    Update ZyLabs_8_10.py

--- Homework#2/ZyLabs_8_10.py
+++ Homework#2/ZyLabs_8_10.py
@@ -1,1 +1,10 @@
 # Name: Huzaifa Shoeb, ID: 1925670
+
+word = str(input())
+joint_word = word.replace(" ", "")
+new = joint_word[::-1]
+
+if joint_word == new:
+    print('{} is a palindrome'.format(word))
+else:
+    print('{} is not a palindrome'.format(word))

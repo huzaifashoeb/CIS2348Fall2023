5a73c5b2cc5dc2fd723ca8162e3339f0f2101804
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 16:39:38 -0500

    Update ZyLabs_7_25.py

--- Homework#2/ZyLabs_7_25.py
+++ Homework#2/ZyLabs_7_25.py
@@ -1,1 +1,13 @@
+# Name: Huzaifa Shoeb, ID: 1925670
 
+amount = int(input())
+
+dollars = amount // 100
+quarters = amount % 100 // 25
+dimes = amount % 100 % 25 // 10
+nickels = amount % 100 % 25 % 10 // 5
+pennies = amount % 100 % 25 % 10 % 5 // 1
+
+if amount <= 0:
+    print("No change")
+  

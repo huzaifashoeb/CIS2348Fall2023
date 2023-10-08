8a0cb68b9664346a127a6290a785675434a5d701
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 14:46:24 -0500

    Update ZyLabs_6_22.py

--- Homework#2/ZyLabs_6_22.py
+++ Homework#2/ZyLabs_6_22.py
@@ -6,3 +6,11 @@ c = int(input())
 d = int(input())
 e = int(input())
 f = int(input())
+
+for x in (-10,10):
+    for y in (-10,10):
+        if (a * x) + (b * y) == c and (d * x) + (e * y) == f:
+            print(x, y)
+        else:
+            print('No solution')
+          

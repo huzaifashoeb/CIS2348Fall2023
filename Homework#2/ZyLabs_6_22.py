5f07b0d759dab2e70647f7cb5b0b3e3a33c24870
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 14:47:51 -0500

    Update ZyLabs_6_22.py

--- Homework#2/ZyLabs_6_22.py
+++ Homework#2/ZyLabs_6_22.py
@@ -7,10 +7,13 @@ d = int(input())
 e = int(input())
 f = int(input())
 
-for x in (-10,10):
-    for y in (-10,10):
+check = False
+
+for x in range(-10, 10):
+    for y in range(-10, 10):
         if (a * x) + (b * y) == c and (d * x) + (e * y) == f:
-            print(x, y)
-        else:
-            print('No solution')
-          
+            check = True
+            print(x,y)
+
+if check == False:
+    print('No solution')

68d17e5cff286d2471f928db090ec5ef68b56154
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 17:41:36 -0500

    Update ZyLabs_7_25.py

--- Homework#2/ZyLabs_7_25.py
+++ Homework#2/ZyLabs_7_25.py
@@ -1,40 +1,48 @@
 # Name: Huzaifa Shoeb, ID: 1925670
 
-ef exact_change():
-    input_val = int(input(''))
+def exact_change(user_total):
+    num_dollars = user_total // 100
+    user_total %= 100
+    num_quarters = user_total // 25
+    user_total %= 25
+    num_dimes = user_total // 10
+    user_total %= 10
+    num_nickels = user_total // 5
+    user_total %= 5
+    num_pennies = user_total
+    return (num_dollars, num_quarters, num_dimes, num_nickels, num_pennies)
+
+
+if __name__ == '__main__':
+    input_val = int(input())
+    num_dollars, num_quarters, num_dimes, num_nickels, num_pennies = exact_change(
+        input_val)
+
     if input_val <= 0:
         print('no change')
 
     else:
-        num_dollars = input_val // 100
-        input_val %= 100
-        num_quarters = input_val // 25
-        input_val %= 25
-        num_dimes = input_val // 10
-        input_val %= 10
-        num_nickels = input_val // 5
-        input_val %= 5
-        num_pennies = input_val
-
         if num_dollars > 1:
             print('%d dollars' % num_dollars)
         elif num_dollars == 1:
             print('%d dollar' % num_dollars)
+
         if num_quarters > 1:
             print('%d quarters' % num_quarters)
         elif num_quarters == 1:
             print('%d quarter' % num_quarters)
+
         if num_dimes > 1:
             print('%d dimes' % num_dimes)
         elif num_dimes == 1:
             print('%d dime' % num_dimes)
+
         if num_nickels > 1:
             print('%d nickels' % num_nickels)
         elif num_nickels == 1:
             print('%d nickel' % num_nickels)
+
         if num_pennies > 1:
             print('%d pennies' % num_pennies)
         elif num_pennies == 1:
             print('%d penny' % num_pennies)
-
-exact_change()

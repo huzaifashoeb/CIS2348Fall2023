fbfa9552c15e3bb2af97cb0a3d8973bcf1fb1259
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 20:52:30 -0500

    Update Coding_Problem_02.py

--- Homework#2/Coding_Problem_02.py
+++ Homework#2/Coding_Problem_02.py
@@ -1,30 +1,28 @@
 #Name: Huzaifa Shoeb, ID: 1925670
-
-# Let's create a dictionary first
-Months = {
-  "January": 1,
-  "February": 2,
-  "March": 3,
-  "April": 4,
-  "May": 5,
-  "June": 6,
-  "July": 7,
-  "August": 8,
-  "September": 9,
-  "October": 10,
-  "November": 11,
-  "December": 12}
-
-# Let's import the 're' module in here
-
 import re
-mydate = input()
-for data in [mydate, -1]
-  if data == -1:
-    break
-  if re.match(r'(\w+) (\d{1,2}), (\d{4})', data) == None:
-    continue
-  output = data.split(" ")
-  output [0] = str(Months[output[0]])
-  output [1] = output[1].replace(",", "")
-  print("/".join(output))
+
+months = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12,
+}
+
+yourdate = input()
+for data in [yourdate, -1]:
+    if data == -1:
+        break
+    if re.match(r'(\w+) (\d{1,2}), (\d{4})', data) is None:
+        continue
+    output = data.split(" ")
+    output[0] = str(months[output[0]])
+    output[1] = output[1].replace(",", "")
+    print("/".join(output))

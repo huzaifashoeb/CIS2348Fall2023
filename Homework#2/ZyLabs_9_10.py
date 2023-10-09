926b390473f6d97074e3bca05d3065308db113d2
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-10-08 19:21:44 -0500

    Update ZyLabs_9_10.py

--- Homework#2/ZyLabs_9_10.py
+++ Homework#2/ZyLabs_9_10.py
@@ -1,1 +1,18 @@
 
+
+import csv
+
+file = input()
+
+with open(file, 'r') as wordsfile:
+    words_reader = csv.reader(wordsfile)
+    for row in words_reader:
+        words_list = row
+
+        no_duplicates = list(dict.fromkeys(words_list))
+        list_length = len(no_duplicates)
+
+        for i in range(list_length):
+            print(no_duplicates[i], words_list.count(no_duplicates[i]))
+            list[i], words_list.count(no_duplicates[i])
+

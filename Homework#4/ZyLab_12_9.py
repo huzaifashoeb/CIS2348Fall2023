6163c493e1205c55b73b142075bd7a69ec4cde01
huzaifashoeb <143118571+huzaifashoeb@users.noreply.github.com>
2023-12-04 16:56:47 -0600

    Create ZyLab_12_9.py

---
+++ Homework#4/ZyLab_12_9.py
@@ -0,0 +1,15 @@
+# Name: Huzaifa Shoeb, ID: 1925670
+
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+
+        age = int(parts[1]) + 1
+    except ValueError:
+
+        age = 0
+    print(f'{name} {age}')
+
+    parts = input().split()
+    name = parts[0]
